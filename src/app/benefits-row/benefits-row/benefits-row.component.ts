14359953485196ecd76dd9f5edb464c1d9f7d3ea
Luana Andrada <luandrada12@live.com>
2020-11-03 10:47:20 -0300

    fila beneficios adaptada a celular sin carousel

--- src/app/benefits-row/benefits-row/benefits-row.component.ts
+++ src/app/benefits-row/benefits-row/benefits-row.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./benefits-row.component.scss']
 })
 export class BenefitsRowComponent implements OnInit {
-
+  // card= document.querySelectorAll(".card")
+  // cantidadPaginas= Math.ceil(this.card.length /1);
   constructor() { }
 
   ngOnInit(): void {
+        /// ****  *** CARUSEL *** ****  ///
+        // const fila=document.getElementById("contenedor-carousel");
+        // const carousel = document.getElementById("carousel")
+        //  carousel.addEventListener("click",()=>{
+        //    fila.scrollLeft += fila.offsetWidth;
+        //  })
+        //  carousel.addEventListener("click",()=>{
+        //   fila.scrollLeft -= fila.offsetWidth;
+        // })
   }
 
-}
+  }
