a79e9f07b65547689e8395635475c1189383b098
Luana Andrada <luandrada12@live.com>
2020-11-09 13:12:18 -0300

    mis compras con menu flotante vista mobile

--- src/app/user-options/mis-compras/mis-compras.component.ts
+++ src/app/user-options/mis-compras/mis-compras.component.ts
@@ -17,21 +17,41 @@ export class MisComprasComponent implements OnInit {
   }
   //// **** *** LATERAL MENU *** **** /////
   showLateralMenu(){
+    if (screen.width>800) {
     let lateralmenu=document.getElementById("lateralMenu");
     lateralmenu.style.width="200px";
     let menu = document.getElementById("lateral-container");
     menu.style.display="block";
-    let arrow= document.getElementById("openMenu");
+    let arrow= document.getElementById("botonMenu");
     arrow.style.display="none"
+    } else{
+     let lateralmenu=document.getElementById("lateralMenu");
+     lateralmenu.style.opacity="0.9"
+     lateralmenu.style.width="100%"
+      let menu = document.getElementById("lateral-container");
+       menu.style.display="block";
+      let close = document.getElementById("close-menu");
+      close.style.display="block";
+      close.style.marginLeft="15px";
+      close.style.fontSize="0.8em";
+      let arrow = document.getElementById("open-menu");
+      arrow.style.display="none"
+    }
   }
   hiddeLateralMenu(){
     let lateralmenu=document.getElementById("lateralMenu");
     lateralmenu.style.width="30px";
     let menu = document.getElementById("lateral-container");
     menu.style.display="none";
-    let arrow= document.getElementById("openMenu");
+    let close = document.getElementById("close-menu");
+    close.style.display="none";
+    let arrow = document.getElementById("open-menu");
     arrow.style.display="block"
   }
+
+
+
+
   showDetail1(){
 
       ///// *** **** MOSTRAR DETALLE COMPRA *** *** /////
