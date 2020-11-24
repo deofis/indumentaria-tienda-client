fab5ff8dc48893e50bdf43d4a3f2d870cc9fa1ff
neritoo <ezegavilan95@gmail.com>
2020-11-24 00:03:49 -0300

    API BASE URL de AWS agregada.

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL        = 'https://deofis-tienda-apirest.herokuapp.com';
+export const API_BASE_URL        = 'https://api.deofisdev.online';
 export const OAUTH2_REDIRECT_URI = 'https://wantfrom-tienda.web.app/oauth2/redirect';
 export const GOOGLE_AUTH_URL     = `${API_BASE_URL}/oauth2/authorize/google?redirect_uri=${OAUTH2_REDIRECT_URI}`;
 export const FACEBOOK_AUTH_URL   = `${API_BASE_URL}/oauth2/authorize/facebook?redirect_uri=${OAUTH2_REDIRECT_URI}`;
