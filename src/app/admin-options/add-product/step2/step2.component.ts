4596b28ffc30cb9e82dcc0d0df80c6b8d65ab4ee
Luana Andrada <luandrada12@live.com>
2020-11-27 13:53:47 -0300

    formulariofuncionando BIEN

--- src/app/admin-options/add-product/step2/step2.component.ts
+++ src/app/admin-options/add-product/step2/step2.component.ts
@@ -1,16 +1,70 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { PropiedadProducto } from 'src/app/products/clases/propiedad-producto';
+import { ValorPropiedadProducto } from 'src/app/products/clases/valor-propiedad-producto';
+import { ProductoService } from '../../producto.service';
+import { ActivatedRoute } from '@angular/router';
+import { Sku } from 'src/app/products/clases/sku';
+import {FormControl} from '@angular/forms';
 @Component({
   selector: 'app-step2',
   templateUrl: './step2.component.html',
   styleUrls: ['./step2.component.scss']
 })
 export class Step2Component implements OnInit {
-
+ properties:PropiedadProducto[];
+ values:ValorPropiedadProducto [];
   oferta:boolean=false;
-  constructor() { }
+  propertyID:number;
+  formSkus:FormGroup;
+  newSku:Sku;
+  propiedades:string="propiedad";
+  constructor(private productoService:ProductoService,
+              private fb:FormBuilder,
+              private activatedroute:ActivatedRoute,) {
+     this.newSku=new Sku();
+   }
 
   ngOnInit(): void {
+    this.getPropertiesOfSubcategory();
+    this.crearForm();
+  }
+ 
+  crearSku(){
+    this.newSku.precio=this.formSkus.controls.precio.value;
+   this.newSku.precioOferta=this.formSkus.controls.precioOferta.value;
+   this.newSku.disponibilidad=this.formSkus.controls.disponibilidad.value;
+   this.newSku.valores=this.formSkus.controls.valores.value;
+   console.log(this.newSku);
+
+   this.productoService.createNewSku(this.newSku,1).subscribe( response => 
+    console.log(response))
+  }
+  crearForm(){
+    this.formSkus=this.fb.group({
+       id:[""],
+       nombre:[""],
+       descripcion:[""],
+       precio:[""],
+       precioOferta:[""],
+       disponibilidad:[""],
+       valoresData:[""],
+       valores:[""],
+       defaultProducto:[""],
+       producto:[""],  
+    });
+  }
+  get valores(){
+    return this.formSkus.get('valores')
+  }
+  get precio(){
+    return this.formSkus.get('precio')
+  }
+  get pcioOferta(){
+    return this.formSkus.get('precioOferta')
+  }
+  get disponibilidad(){
+    return this.formSkus.get('disponibilidad')
   }
   precioOferta(){
     let ofertaInput= document.getElementById("inputOfertaSku");
@@ -26,4 +80,12 @@ export class Step2Component implements OnInit {
   addProperty(){
     
   }
+  getPropertiesOfSubcategory(){
+    // this.activatedroute.params.subscribe(param=> {
+    //   let subcategory= param.id;
+    this.productoService.getPropertiesOfSubcategory(1).subscribe((response: any) => {
+      this.properties=response;
+    })
+  }
+
 }
--- src/app/admin-options/producto.service.ts
+++ src/app/admin-options/producto.service.ts
@@ -4,6 +4,9 @@ import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { API_BASE_URL } from '../config/config';
 import { Producto } from '../products/clases/producto';
+import { PropiedadProducto } from '../products/clases/propiedad-producto';
+import { ValorPropiedadProducto } from '../products/clases/valor-propiedad-producto';
+import { Sku } from '../products/clases/sku';
 
 @Injectable({
   providedIn: 'root'
@@ -18,5 +21,22 @@ url:string=API_BASE_URL+"/api";
       map((response:any) =>response.producto as Producto)
     )
   }
+
+  
+  getAllProperties():Observable<PropiedadProducto[]>{
+    return this.http.get(`${this.url}/productos/propiedades`).pipe( map( response => response as PropiedadProducto[]));
+  
+  }
+  getPropertiesOfSubcategory(subcategoriaId:number):Observable<PropiedadProducto[]>{
+    return this.http.get(`${this.url}/subcategorias/${subcategoriaId}/propiedades`).pipe( map( (response:any)=> response.propiedades as PropiedadProducto[]));
+  
+  }
+  createNewSku(newSku:Sku, productoId:number):Observable<Sku>{
+    return this.http.post(`${this.url}/productos/${productoId}/skus`,newSku).pipe(
+      map((response:any) =>response )
+    )
+  }
+
 }
+
  
