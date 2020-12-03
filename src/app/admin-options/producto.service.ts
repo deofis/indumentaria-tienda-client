b8bb6550db897ca8039296a266a6dcf069d4c2c1
lucas <lucas@LAPTOP-UBDPGNP8>
2020-12-03 16:42:00 -0300

    pantallas-promociones

--- src/app/admin-options/producto.service.ts
+++ src/app/admin-options/producto.service.ts
@@ -4,6 +4,7 @@ import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { API_BASE_URL } from '../config/config';
 import { Producto } from '../products/clases/producto';
+import { Promocion } from './admin-promos/clases/promocion';
 
 @Injectable({
   providedIn: 'root'
@@ -18,5 +19,16 @@ url:string=API_BASE_URL+"/api";
       map((response:any) =>response.producto as Producto)
     )
   }
+
+  crearNewPromotionSub(promocion:Promocion, id:number){
+    return this.http.post(`${this.url}/subcategorias/${id}/promociones`, promocion);
+  };
+
+  createNewPromotionProducto(promocion: Promocion, id:number){
+    return this.http.post(`${this.url}/productos/${id}/promociones`, promocion);
+  };
+
+
+
 }
  
--- src/app/log-in/services/validadores.service.ts
+++ src/app/log-in/services/validadores.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
@@ -19,5 +19,6 @@ export class ValidadoresService {
         pass2Control.setErrors({ noIgual: true })};
       }
     }
+
 
 }
