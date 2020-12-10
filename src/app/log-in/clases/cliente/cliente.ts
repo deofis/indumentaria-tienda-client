c58dc865c5201e1786d40987a85f1ed3148296ea
Luandrada <luandrada12@live.com>
2020-12-10 12:18:00 -0300

    form login bootstrap grid

--- src/app/log-in/clases/cliente/cliente.ts
+++ src/app/log-in/clases/cliente/cliente.ts
@@ -2,10 +2,11 @@ import { Direccion } from './direccion';
 
 export class Cliente {
     id: number;
-    idPersona: number;
+    dni: number;
     nombre: string;
     apellido: string;
     email: string;
     telefono: string;
+    fechaNacimiento:string;
     direccion: Direccion;
 }
