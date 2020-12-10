f7a42b58a48f43d79fb7343ba4135549f74c8e4d
lucas <lucas@LAPTOP-UBDPGNP8>
2020-12-10 17:57:22 -0300

    Cambios panle admin - Funcionalidad marcas

--- src/app/admin-options/brands-panel/brands-panel.component.ts
+++ src/app/admin-options/brands-panel/brands-panel.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/log-in/services/auth.service';
+import { Marca } from 'src/app/products/clases/marca';
+import { MarcasService } from '../marcas.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-brands-panel',
@@ -8,32 +13,66 @@ import { AuthService } from 'src/app/log-in/services/auth.service';
   styleUrls: ['./brands-panel.component.scss']
 })
 export class BrandsPanelComponent implements OnInit {
-  brands:any[]=[ "Samsung","Iphone","Motorola","Nokia","Lenovo"]
-  
-  constructor( private router:Router,private authService: AuthService,) { }
+
+  brands: Marca[] = [];
+  formMarca: FormGroup;
+  newBrand: Marca;
+
+  constructor( private router:Router,
+               private authService: AuthService,
+               private marcasService: MarcasService,
+               private modalService: NgbModal,
+               private fb: FormBuilder) { }
 
   ngOnInit(): void {
+
+    this.getBrands();
+    this.newBrand = new Marca();
+    this.crearFormulario();
+    
   
   }
-/// *** **** NEW BRAND **** **** ///
-    showInput(){
-    let input =   document.getElementById("newBrand")
-    input.style.display="flex";
-    input.style.justifyContent="space-between";
-    input.style.alignItems="center";
-    let option= document.getElementById("add");
-    option.style.display="none"
+
+  getBrands(){
+    this.marcasService.getBrands().subscribe((resp:any) => {
+      this.brands = resp;
+    })
+  };
+
+  openModal(longContent) {
+
+    this.modalService.open(longContent, { scrollable: true, centered: true });
+    
+  };
+
+  openModalModify(modifyContent){
+    this.modalService.open(modifyContent, { scrollable: true, centered: true})
   }
 
-  addBrand(brand:string):void {
-    brand=brand.toLowerCase().replace(/\b[a-z]/g, function(letter) {
-      return letter.toUpperCase();
-      });
-      
-   this.brands.push(brand);
-   let input = document.getElementById("input") as HTMLInputElement ;
-   input.value=" ";
-   
-  }
+  crearFormulario(){
+
+    this.formMarca = this.fb.group({
+      nombre: ['', Validators.required]
+    });
+
+  };
+
+  get nombreNoValido(){
+    return this.formMarca.get('nombre').invalid && this.formMarca.get('nombre').touched
+  };
+
+  crearMarca(){
+
+    this.newBrand.nombre = this.formMarca.controls.nombre.value;
+    console.log(this.newBrand);
+
+    this.marcasService.createNewBrand(this.newBrand).subscribe((resp:any) => {
+      console.log(resp);
+    });
+
+    this.formMarca.reset();
+    
+    
+  };
 
 }
---
+++ src/app/admin-options/marcas.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpEvent, HttpParams, HttpRequest } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { API_BASE_URL } from '../config/config';
+import { Marca } from '../products/clases/marca';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MarcasService {
+
+  url:string=API_BASE_URL+"/api";
+
+  constructor( private http: HttpClient ) { }
+
+  getBrands(){
+
+    return this.http.get(`${this.url}/productos/marcas`).pipe(map((resp:any) => {
+      return resp.marcas
+    }))
+
+  };
+
+  createNewBrand(brand: Marca){
+
+    return this.http.post(`${this.url}/productos/marcas/nueva`, brand);
+
+  };
+
+  modifyBrand(brand: Marca, id: number){
+
+    return this.http.put(`${this.url}/productos/marcas/${id}`, brand);
+
+  };
+
+}
