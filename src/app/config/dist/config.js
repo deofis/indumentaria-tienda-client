7d829a1af88d1a9ef10aeb02bb012ed97f7d0739
neritoo <ezegavilan95@gmail.com>
2020-12-18 15:48:37 -0300

    Borrados Styles y dist.

--- src/app/config/dist/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.FACEBOOK_AUTH_URL = exports.GOOGLE_AUTH_URL = exports.OAUTH2_REDIRECT_URI = exports.API_BASE_URL = void 0;
-exports.API_BASE_URL = 'https://api.deofisdev.online';
-exports.OAUTH2_REDIRECT_URI = 'https://wantfrom-tienda.web.app/oauth2/redirect';
-exports.GOOGLE_AUTH_URL = exports.API_BASE_URL + "/oauth2/authorize/google?redirect_uri=" + exports.OAUTH2_REDIRECT_URI;
-exports.FACEBOOK_AUTH_URL = exports.API_BASE_URL + "/oauth2/authorize/facebook?redirect_uri=" + exports.OAUTH2_REDIRECT_URI;
